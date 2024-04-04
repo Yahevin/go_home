d5cf6c760ab6afd51438792642139cb4db3b55f2
Yahevin <Yahevin15@gmail.com>
2024-04-04 22:00:25 +0300

    add scale animation for settings

--- src/components/SideMenu/styles.ts
+++ src/components/SideMenu/styles.ts
@@ -1,11 +1,28 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { SxProps } from '@mui/system/styleFunctionSx/styleFunctionSx';
 import colors from 'src/constants/colors';
+
+const scale = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  10% {
+    transform: scale(1.25);
+  }
+  25% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
 
 export const ButtonPosition = styled.div`
   position: absolute;
   top: 10px;
   right: 5px;
+
+  animation: ${scale} 4s ease-in 3s infinite;
 `;
 
 export const BoxSX: SxProps = { width: 220, height: '100%' };
